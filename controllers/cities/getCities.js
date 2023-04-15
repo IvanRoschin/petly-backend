6f883f9629297d99de2f477051b31caa8fb08802
Ivan Roschin <ivan.roschin86@gmail.com>
2023-04-15 11:25:11 +0300

    update ctrl getCities

--- controllers/cities/getCities.js
+++ controllers/cities/getCities.js
@@ -1,20 +1,51 @@
 const { City } = require("../../models");
 
+// const getCities = async (req, res) => {
+//   const { query = "" } = req.query;
+//   // const regexp = `/^${query}/`;
+//   // console.log(regexp);
+//   const language = req.query.lang;
+//   console.log("language", language);
+
+//   const city = await City.find(
+//     { cityEn: { $regex: query, $options: "i" } },
+//     "stateEn cityEn countyEn useCounty"
+//   );
+
+//   // console.log("city", city);
+//   // console.log(city.map((item) => console.log("item", item.city)));
+
+//   if (!city) {
+//     res.json({ results: "Sorry, city not found" });
+//   }
+
+//   res.json(city);
+// };
+
 const getCities = async (req, res) => {
   const { query = "" } = req.query;
-  // const regexp = `/^${query}/`;
-  // console.log(regexp);
+  const language = req.query.lang;
 
-  const city = await City.find(
-    { cityEn: { $regex: query, $options: "i" } },
-    "stateEn cityEn countyEn useCounty"
-  );
+  let city;
 
-  // console.log("city", city);
-  // console.log(city.map((item) => console.log("item", item.city)));
+  if (language === "uk") {
+    city = await City.find(
+      { city: { $regex: query, $options: "i" } },
+      "state county city type stateEn countyEn cityEn useCounty"
+    );
+  } else if (language === "en") {
+    city = await City.find(
+      { cityEn: { $regex: query, $options: "i" } },
+      "state county city type stateEn countyEn cityEn useCounty"
+    );
+  } else {
+    res.status(400).json({ error: "Invalid language" });
+    return;
+  }
 
-  if (!city) {
-    res.json({ results: "Sorry, city not found" });
+  if (city.length === 0) {
+    res.status(404).json({ error: "City not found" });
+    return;
   }
 
   res.json(city);
